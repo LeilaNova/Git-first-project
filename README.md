8770ed5ba4c2bd1f6ae68154bdc12601867624ef
LeilaNova <elektrrra@list.ru>
2023-05-30 18:20:22 +0300

    Add class Main and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+## Work
+*good sales = good salary* 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = new int[]{5, 4, 500, 900};
+        SalesManager salesManager = new SalesManager(sales);
+        int max = salesManager.max();
+        System.out.println("Наибольшая продажа: " + max);
     }
 }
