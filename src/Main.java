b16a296b1d2e2d12bd5f1387407b55acccf1603a
LeilaNova <elektrrra@list.ru>
2023-05-31 18:11:33 +0300

    Create min and average methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ public class Main {
         int[] sales = new int[]{5, 4, 500, 900};
         SalesManager salesManager = new SalesManager(sales);
         int max = salesManager.max();
+        int min = salesManager.min();
+        int average = salesManager.average();
         System.out.println("Наибольшая продажа: " + max);
+        System.out.println("Наименьшая продажа: " + min);
+        System.out.println("Среднее значение: " + average);
     }
 }
